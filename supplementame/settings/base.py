3f35c361b17d775f80276103a4b1206655918cf9
ArboWeb <arnaudb@netc.fr>
2025-02-12 11:55:11 +0100

    static and media directories modification

--- supplementame/settings/base.py
+++ supplementame/settings/base.py
@@ -167,15 +167,20 @@ STATICFILES_DIRS = [
     os.path.join(PROJECT_DIR, "static"),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-if "DJANGO_STATIC_ROOT" in os.environ:
-    STATIC_ROOT = env("DJANGO_STATIC_ROOT")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# if "DJANGO_STATIC_ROOT" in os.environ:
+#     STATIC_ROOT = env("DJANGO_STATIC_ROOT")
+
+STATIC_ROOT = env("DJANGO_STATIC_ROOT")
 STATIC_URL = "/static/"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-if "DJANGO_MEDIA_ROOT" in os.environ:
-    MEDIA_ROOT = env("DJANGO_MEDIA_ROOT")
+# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+# if "DJANGO_MEDIA_ROOT" in os.environ:
+#     MEDIA_ROOT = env("DJANGO_MEDIA_ROOT")
+
+MEDIA_ROOT = env("DJANGO_MEDIA_ROOT")
 MEDIA_URL = "/media/"
+
 
 # Default storage settings, with the staticfiles storage updated.
 # See https://docs.djangoproject.com/en/5.1/ref/settings/#std-setting-STORAGES
