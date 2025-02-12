50a33735ef384d4440ca8b28b06845397e3f7758
ArboWeb <arnaudb@netc.fr>
2025-02-12 15:44:16 +0100

    settings config

--- supplementame/settings/production.py
+++ supplementame/settings/production.py
@@ -1,4 +1,10 @@
 from .base import *
+from .base import env
+
+
+SECRET_KEY = env("DJANGO_SECRET_KEY")
+
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["supplementame.eu"])
 
 DEBUG = False
 
--- supplementame/wsgi.py
+++ supplementame/wsgi.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from supplementame import settings
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "supplementame.settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "supplementame.settings.dev")
 
 application = get_wsgi_application()
