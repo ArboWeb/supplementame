8cfe39f149d0191e1b27e1c6512eb23821cc2138
ArboWeb <arnaudb@netc.fr>
2025-02-12 13:39:57 +0100

    wsgi modification

--- supplementame/wsgi.py
+++ supplementame/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "supplementame.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "supplementame.settings.production")
 
 application = get_wsgi_application()
